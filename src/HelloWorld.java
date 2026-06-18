ee1aa65b3ad09feb3eb1f7180c0c44368f0a8298
kristinaprokofeva884-crypto <kristinaprokofeva884@gmail.com>
2026-06-19 00:07:22 +0300

    Решение задачи 1.3.3 - символ Unicode

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,20 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Проверка метода charExpression
+        char result = charExpression(5);
+        System.out.println("Символ после 'a' на расстоянии 5: " + result); // 'f'
+
+        char result2 = charExpression(0);
+        System.out.println("Символ после 'a' на расстоянии 0: " + result2);
+
+        char result3 = charExpression(25);
+        System.out.println("Символ после 'a' на расстоянии 25: " + result3);
     }
+
+    //ЗАДАЧА 1.3.3: Символ Unicod
+    public static char charExpression(int a) {
+return(char)('a'+ a);
 }
+}
+
+
