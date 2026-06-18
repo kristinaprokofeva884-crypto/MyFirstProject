c77f5bca58cd0d09dd27e0c52ede2516331d05d8
kristinaprokofeva884-crypto <kristinaprokofeva884@gmail.com>
2026-06-18 15:20:32 +0300

    Добавлен метод priceCalculation

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        HelloWorld hw = new HelloWorld();
+        double result = hw.priceCalculation(8.50, 2);
+        System.out.println("Стоимость товара: " + result);
     }
 
     public int drawsMonitorsCounter(int monitors, int programmers) {
         return monitors % programmers;
+    }
+
+    public double priceCalculation(double price, int count) {
+        return price * count;
     }
 }
