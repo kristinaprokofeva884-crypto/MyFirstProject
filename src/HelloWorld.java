44a3fd5e29f02d2440afc9c5f6a939841b38c9d5
kristinaprokofeva884-crypto <kristinaprokofeva884@gmail.com>
2026-06-18 14:46:04 +0300

    Решение Практической_задачи_1.2.5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
     public int drawsMonitorsCounter(int monitors, int programmers) {
         return monitors % programmers;
     }
 }
-
