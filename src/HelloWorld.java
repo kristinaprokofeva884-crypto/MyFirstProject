38258b854582006320fabb29584ca1be98a6e6cd
kristinaprokofeva884-crypto <kristinaprokofeva884@gmail.com>
2026-06-18 22:39:01 +0300

    Практическая_задача_1.2.6 - примитивы типы

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,23 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Задача 1.2.6: Объявление всех 8 примитивных типов
+        byte varBate = 127;
+        short varShort = 32000;
+        int varInt = 1000000;
+        long varLong = 10000000000L;
+        float varFloat = 3.14f;
+        double varDouble = 3.1415926535;
+        boolean varBoolean = true;
+        char varChar = 'А';
+
+        System.out.println("byte: " + varBate);
+        System.out.println("short: " + varShort);
+        System.out.println("int: " + varInt);
+        System.out.println("long: " + varLong);
+        System.out.println("float: " + varFloat);
+        System.out.println("double: " + varDouble);
+        System.out.println("boolean: " + varBoolean);
+        System.out.println("char: " + varChar);
     }
 }
+
