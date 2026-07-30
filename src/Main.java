e83ab2f0eb6a410c13496807b1591530eda870cd
kristinaprokofeva884-crypto <kristinaprokofeva884@gmail.com>
2026-07-30 21:18:02 +0300

    Практическая_задача_5.1.6 - UnaryOperator

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,27 @@
-//Подключаем класс ObjectMapper - главный инструмент для работы с JSON
-//Он умеет превращать JSON в объекты Java и обратно
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-//Создаем внутринний класс CreateUserRequest с полями по суловию задачи
-class CreateUserRequest {
-    //Создаем классу приватное поле name в тип String
-    private String name;
-    //Создаем классу приватное поле job в тип String
-    private String job;
-
-    //Обращаемся к онструктору без параметров,он нужен Джексону,чтобы создать пустой объект.
-    //После
-    public CreateUserRequest() {
-    }
-
-    //Обращаемся теперь к конструктору с параметрами,он нужен нам,чтобы сразу создать объект с данными
-    //через сеттеры заполнить его полями из JSON.
-    public CreateUserRequest(String name, String job) {
-        //Мы берём ссылку this на поле name текущего объекта и присваиваем
-        //ему значение параметра name, который пришёл в конструктор.
-        this.name = name;
-        //Также делаем с полем job
-        this.job = job;
-    }//Закрваем конструктор с парметрами
-
-    //Далее вход выдвигаются Геттеры и Сеттеры
-//Геттер помогает прочитать поле(которое есть в классе CreateUserRequest.Он приоткрыввется пиватное поле,
-//для чтения кто использует гет и возращает его на место.
-    public String getName() {
-        return name;
-    }
-
-    //Сеттер ничего не возращает,но он может изменить и присвоить ноевое значение для поля name
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    //Также дл другого поля мы используем Геттер и Сеттер
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-
+import java.util.function.UnaryOperator; // Импортируем интерфейс UnaryOperator
+//Сощдаем наш класс с которым будем работать
+class MyOperator {
+    //Пишем нашу сигнатуру из здачи,где будеи понятно с каким методом и параметроммы будем работать.
+    public UnaryOperator<Integer> sqrt() {
+        //Затем мы создаем лямбу и возращаем ее (береv один объект и умножает его на себя)
+        //параметр один x с типом Integer ,а тело функции x * x
+        //Java уже автоматически определяет,что это UnaryOperator<Integer>
+        return x -> x * x;
     }
 }
+    public class Main {
+        //Дальше в классе main мы используем это метод
+        public static void main(String[] args) {
+            // Создаем объект,чтобы вызвать наш метод
+            MyOperator operator = new MyOperator();
+            // Получаем реализацию UnaryOperator
+            UnaryOperator<Integer> squareOperator = operator.sqrt();
+            // apply - метод который существует у калькулятора squareOperator,
+            // он помогает нам ВЫЧИСЛИТЬ (не сложить) результат
+            int result = squareOperator.apply(5);
+            //Выводим результаты проверки в консоль
+            System.out.println("Квадрат числа: " + result);
 
-class UserService {
-    //Внутри передаем метод buildRequestBody-который создает JSON-строку из параметров объекта
-    //buildRequestBody - имя метода: "построить тело запроса".
-    //throws Exception - метод может выбросить исключение (если что-то пойдёт не так).
-    public String buildRequestBody(String name, String job) throws Exception {
-        //1.Создаём объект CreateUserRequest с переданными данными(его конструктором)
-        // Передаём в него имя (name) и должность (job),которые пришли в метод.
-        CreateUserRequest request = new CreateUserRequest(name, job);
-        //2.Создаём ObjectMapper - объект умеет превращать Java-объекты в JSON и обратно
-        //mapper - просто переменная
-        ObjectMapper mapper = new ObjectMapper();
-        //3.Превращаем объект в JSON-строку
-        // .writeValueAsString() — это метод, который превращает Java-объект в JSON-строку и возвращаем
-        //через return метод для request
-        return mapper.writeValueAsString(request);
+        }
     }
-}
-
-public class Main {
-    // Точка входа в программу.Выполнение программы и вывод в консоль
-    public static void main(String[] args) throws Exception {
-        // 1. Создаём объект UserService
-        UserService userService = new UserService();
-        // 2. Вызываем метод через объект
-        String json = userService.buildRequestBody("Анна", "QA Engineer");
-        //Теперь выводим значения на экран переменной,в которой лежат все конкретные значения
-        System.out.println(json);
-    }
-}
 
